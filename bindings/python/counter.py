d57c2df13f5bf7cf6f81b60793bfc480efcf2016
brianprats <biprats@gmail.com>
2018-01-22 19:28:50 +0000

    works for real

--- bindings/python/counter.py
+++ bindings/python/counter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+print('works')
 import time
 import sys
 import requests
@@ -10,25 +12,27 @@ class Counter(object):
     myUrl = 'https://graph.facebook.com/https://www.facebook.com/Thrillist/?fields=engagement'
     head = {'Authorization': 'OAuth {}'.format(myToken)}
     response = requests.get(myUrl, headers=head)
-    self.count = response.json()['engagement']['count']
+    self.count = str(response.json()['engagement']['count'])
     #self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
   def run(self):
     offscreen_canvas = self.matrix.CreateFrameCanvas()
     font = graphics.Font()
-    font.LoadFont("../../../fonts/7x13.bdf")
+    font.LoadFont("../../fonts/9x18.bdf")
     textColor = graphics.Color(255, 255, 0)
-    pos = offscreen_canvas.width
+    pos = 0
+    #pos = offscreen_canvas.width
     while True:
       self.get_likes()
-      now = time.time()
-      while now-time.time() <= 30:
+      print(self.count)
+      start = time.time()
+      while time.time()-start <= 10:
           offscreen_canvas.Clear()
           len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, self.count)
-          pos -= 1
-          if (pos + len < 0):
-              pos = offscreen_canvas.width
 
+      #   pos -= 1
+      #    if (pos + len < 0):
+      #       pos = offscreen_canvas.width
           time.sleep(0.05)
           offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
@@ -56,8 +60,4 @@ class Counter(object):
       print("Exiting\n")
       sys.exit(0)
 
-# Main function
-if __name__ == "__main__":
-    run_text = Counter()
-    if (not run_text.process()):
-        run_text.print_help()
+Counter().process()
